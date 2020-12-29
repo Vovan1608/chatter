151c31eab9c8c72d599fd89e2ee70c6d1bea9ef6
Vovan1608 <vovan1608@gmail.com>
2020-12-29 14:34:17 +0200

    create chat

--- websockets-server.js
+++ websockets-server.js
@@ -5,13 +5,26 @@ var ws = new WebSocketServer({
 	port: port
 });
 
+var messages = [];
+
 console.log('websockets server started');
 
 ws.on('connection', function (socket) {
 	console.log('client connection established');
 
+	messages.forEach(function (msg) {
+		socket.send(msg);
+	});
+
 	socket.on('message', function (data) {
 		console.log('message received: ' + data);
-		socket.send(data);
+		messages.push(data);
+
+		ws.clients.forEach(function (clientSocket) {
+			clientSocket.send(data);
+		});
 	});
 });
+
+// npm run dev -> start sserver
+// wscat --connect ws://localhost:3001 --> connect to chat
